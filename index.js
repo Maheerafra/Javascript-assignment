eb394eb9822b4c1eca9f7051f45ef34e29d4303d
Maheerafra <abdiqafardiriyeafra@gmail.com>
2022-12-14 17:05:55 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+
+console.log(23 + 97)
+
+console.log(23 + 97 + 20 + 30 + 40 + 50 + 10)
+
+let x = 4+6+9
+
+console.log(x)
+
+let z = x/77
+
+console.log(z)
+
+//console.log(4 + 6 + 9) / 77
+
+console.log(10/2)
+
+//variables
+
+let a = 10;
+
+console.log(a)
+
+console.log(9*a)
+
+let b = 7 * a;
+
+console.log(b)
+
+let max = 57;
+
+console.log(max)
+
+let actual = max -13;
+
+console.log(actual)
+
+let percentage = actual/max;
+
+console.log(percentage)
